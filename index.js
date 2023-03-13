ed4e2243e41f25927ea76e24a9a71fd7fa91f665
Samuel <samnmbuguah@gmail.com>
2023-03-13 06:36:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Write your code here!
+const main = document.querySelector('#main');
+
+main.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.textContent = 'Mbugua is the champion';
+
+newHeader.setAttribute('id', 'victory');
+
+const body = document.querySelector(body);
+
+body.appendChild(newHeader);
+
